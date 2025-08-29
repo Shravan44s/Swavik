cf6b57ed78518014c422cb9c58e34b0db8618bec
Shravan Hajare <shravanhajare@Shravans-MacBook-Air.local>
2025-08-30 03:15:47 +0530

    Your informative commit message

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:5000',
+  baseURL: process.env.REACT_APP_API_URL || 'http://127.0.0.1:5000',
 });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if(token) {
+  if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;

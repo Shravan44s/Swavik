64897bb63de9cfc4191af0adefe3855c96f4c9a0
Shravan Hajare <shravanhajare@Shravans-MacBook-Air.local>
2025-08-31 04:22:57 +0530

    title

--- backend/utils/db.py
+++ backend/utils/db.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host='swavik.crs88o8ew1b9.ap-south-1.rds.amazonaws.com',
+        host='localhost',
         user='root',
         password='11082002',
         database='swavik_db'

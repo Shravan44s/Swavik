8c8d0854f2fc120c8b3c2a4b38cea1c883ea3e58
Shravan Hajare <shravanhajare@Shravans-MacBook-Air.local>
2025-09-15 09:53:07 +0530

    changed main

--- backend/app.py
+++ backend/app.py
@@ -23,5 +23,6 @@ app.register_blueprint(auth_bp)
 app.register_blueprint(user_bp)
 app.register_blueprint(course_bp)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+

efd664240ea8b9fecd06afd5f8cd3deaa0c349c5
Shravan Hajare <shravanhajare@Shravans-MacBook-Air.local>
2025-09-15 11:04:10 +0530

    api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: process.env.NODE_ENV === "production"
-    ? "http://43.204.28.84:8000"
+    ? "http://43.204.28.84:8000" || "https://api.swavik.co.in"
     : "http://127.0.0.1:5000",
 });
 
